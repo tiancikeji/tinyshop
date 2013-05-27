af2a3cef364e580f0c7a0918607e2a14fc83186e
王雷 <wanglei@chinacache.com>
2013-05-27 14:39:29 +0800

    update gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyshop::Application.config.secret_token = 'e830ddef4eba0f0f9b6f8971559b8c6684ad77b2761660e7600b098d36046f0241f8fcd7607657e14b1afc95c1fbd31fed463da2a8d82bb67fccaf6f1a2e34ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tinyshop::Application.config.session_store :cookie_store, key: '_tinyshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tinyshop::Application.config.session_store :active_record_store
